5456c729b59ddbbb03b9881a8a9de58b10897e85
ERSİN ÇÖL <ersincol235@gmail.com>
2022-04-29 00:01:42 +0300

    rekürsif fonksiyon ile faktöriyel çalışması

--- Recursion.java
+++ Recursion.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Recursion {
+import java.util.Scanner;
+
+public class Recursion {
+    public static void main(String args[]){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number=scanner.nextInt();
+        System.out.print("Faktöriyel: "+factorial(number));
+    }
+    public static int factorial(int number){
+        if(number==1){
+            return 1;
+        }
+        return number*factorial(number-1);
+    }
 }
